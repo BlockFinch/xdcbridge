34b744fd48f55e871f3f7de34dba09b2cd5f3433
BlockFinch <99175113+BlockFinch@users.noreply.github.com>
2022-03-24 19:31:43 +0530

    Update History.js

--- src/Modules/History/History.js
+++ src/Modules/History/History.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Tabs from "react-bootstrap/Tabs";
+import { useState, useEffect } from 'react';
 
 import { Tab } from "react-bootstrap";
 import Faq from "react-faq-component";
